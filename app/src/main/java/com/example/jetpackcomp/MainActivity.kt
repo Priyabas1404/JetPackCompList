68ab2f7c09b7bd5b5c060f00c1eb28c7258c7cad
Priyabas1404 <109682693+Priyabas1404@users.noreply.github.com>
2024-03-27 16:22:12 -0500

    Initial Commit

--- app/src/main/java/com/example/jetpackcomp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -11,51 +13,59 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CounterScreen()
+            ListScreen()
         }
     }
 
 
     @Composable
-    fun CounterScreen() {
-        var count by remember { mutableStateOf(0) }
-
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(text = "Jetpack Compose Counter") }
+    fun ListScreen() {
+        var city_name by remember { mutableStateOf("") }
+        var city_names by remember { mutableStateOf(listOf<String>()) }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                OutlinedTextField(
+                    value = city_name,
+                    onValueChange = { newtext ->
+                        city_name = newtext
+                    },
+                    modifier = Modifier.weight(1f)
                 )
-            },
-            content = {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = "Counter: $count",
-                        style = MaterialTheme.typography.h4,
-                        color = Color.Black
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = { count++ }) {
-                        Text(text = "Increment")
-                    }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(onClick = {
+                    if (city_name.isNotBlank())
+                        city_names += city_name
+                        city_name = ""
+                }) {
+                    Text(text = "Add")
                 }
             }
-        )
+            LazyColumn {
+                items(city_names) { currentcityname ->
+                    Text(text = currentcityname,
+                        modifier = Modifier.fillMaxWidth().padding(16.dp)
+                    )
+                    Divider()
+                }
+            }
+        }
     }
 
     @Preview
     @Composable
-    fun PreviewCounterScreen() {
-        CounterScreen()
+    fun PreviewListScreen() {
+        ListScreen()
     }
 }
